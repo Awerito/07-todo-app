dbc96093fdbde958247f5c2a818f873c1d4c31a8
Awerito <diego@openlink.cl>
2024-09-15 00:29:35 -0300

    Add: enter key press and submit reset form

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,9 +2,17 @@ import { Button, TextInput, Group } from '@mantine/core';
 import { useField } from '@mantine/form';
 
 function TodoForm({ addTask }) {
+  const validateInput = (value) => {
+    if (value.trim() === '') {
+      return 'Task cannot be empty';
+    }
+
+    addTask(value);
+    field.reset();
+  }
   const field = useField({
     initialValue: '',
-    validate: (value) => value.trim() !== '' ? addTask(value) : "Task cannot be empty",
+    validate: (value) => value.trim() !== '' ? validateInput(value) : 'Task cannot be empty',
   });
 
   return (
@@ -15,6 +23,7 @@ function TodoForm({ addTask }) {
         mb="md"
         flex={1}
         style={{ marginTop: 17 }}
+        onKeyDown={(event) => event.key === 'Enter' && field.validate()}
       />
       <Button onClick={field.validate}>Add task</Button>
     </Group>
